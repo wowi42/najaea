9494c3017ef9c96e106c27f892662dbf5778d0a1
Loïc Tosser <loic.t@advance.ae>
2017-09-27 19:48:15 +0400

    Changintg str

--- kpi/models.py
+++ kpi/models.py
@@ -3,8 +3,13 @@ from django.db import models
 class Employee(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
-    
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
 class Result(models.Model):
     employee = models.ForeignKey(Employee)
     created_at = models.DateTimeField('date created')
     mark = models.IntegerField(default=0)
+    def __str__(self):
+        return self.employee.first_name + " " + self.employee.last_name + " " + str(self.mark)
